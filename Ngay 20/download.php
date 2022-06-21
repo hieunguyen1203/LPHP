af870ff17fdd54d3e1b003ad6308be320ee11ba4
hieunguyen1203 <hieunguyen1203@outlook.com>
2022-06-21 20:01:17 +0700

    ok

--- Ngay 20/download.php
+++ Ngay 20/download.php
@@ -10,12 +10,13 @@ if(isset($_REQUEST["file"])){
     // Process download
     if(file_exists($filepath)) {
         header('Content-Description: File Transfer');
-        header('Content-Type: application/octet-stream');
-        header('Content-Disposition: attachment; filename="'.basename($filepath).'"');
-        header('Expires: 0');
-        header('Cache-Control: must-revalidate');
-        header('Pragma: public');
-        header('Content-Length: ' . filesize($filepath));
+        //header('Content-Type: application/octet-stream');
+        header('Content-Type: text/html');
+        //header('Content-Disposition: attachment; filename="'.basename($filepath).'"');
+//        header('Expires: 0');
+//        header('Cache-Control: must-revalidate');
+//        header('Pragma: public');
+//        header('Content-Length: ' . filesize($filepath));
         flush(); // Flush system output buffer
         readfile($filepath);
         die();
