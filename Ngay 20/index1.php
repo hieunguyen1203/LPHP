835e71710753cdd87c50adcac46d20c084c1a0f5
hieunguyen1203 <hieunguyen1203@outlook.com>
2022-06-21 19:37:53 +0700

    wqdsx

--- Ngay 20/index1.php
+++ Ngay 20/index1.php
@@ -6,8 +6,24 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <style>
+        .img-box{
+            display: inline-block;
+            text-align: center;
+            margin: 0 15px;
+        }
+    </style>
 </head>
 <body>
+<?php
+    $images = ["kites.jpg", "balloons.jpg"];
+    foreach ($images as $image) {
+        echo "<div class=\"img-box\">";
+        echo "<img src='images/$image' width='200' alt='".pathinfo($image, PATHINFO_EXTENSION)."'>";
+        echo '<p><a href="download.php?file=' . urlencode($image) . '">Download</a></p>';
+        echo "</div>";
+    }
+?>
 
 </body>
 </html>
