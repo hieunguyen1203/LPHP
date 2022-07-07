f7d1b8e05141521d0fac356b7fa0dd16f0e8fb4a
hieunguyen1203 <hieunguyen1203@outlook.com>
2022-07-07 20:32:43 +0700

    OK

--- Ngay23/index9.php
+++ Ngay23/index9.php
@@ -2,4 +2,10 @@
 require_once "index8.php";
 require_once "index8b.php";
 
-use Dat\demoClass;
+use \Dat\demoClass as demoClassA;
+use \TuanAnh\demoClass as demoClassB;
+$a = new demoClassA();
+$a->info();
+
+$b = new demoClassB();
+$b->info();
