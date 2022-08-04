cc58004abc0e3a937547bb472f510986d2968e71
hieunguyen1203 <hieunguyen1203@outlook.com>
2022-08-04 19:00:08 +0700

    OK

--- Ngay30/books/mvc/controllers/BookController.php
+++ Ngay30/books/mvc/controllers/BookController.php
@@ -1,6 +1,36 @@
 <?php
 namespace MVC\Controllers;
 
-class BookController {
+class BookController
+{
+    /*
+     * liệt kê ra các bản ghi
+    */
+    public function indexAction()
+    {
 
+    }
+    /*
+     * trả về view thêm mới book
+    */
+    public function addAction()
+    {
+
+    }
+
+    public function insertAction()
+    {
+
+    }
+    /*
+     * trả về view sửa book
+    */
+    public function editAction()
+    {
+
+    }
+    public function updateAction()
+    {
+
+    }
 }
--- Ngay30/books/mvc/models/BookModel.php
+++ Ngay30/books/mvc/models/BookModel.php
@@ -1,6 +1,20 @@
 <?php
 namespace MVC\Models;
 
-class BookModel {
+class BookModel extends Connect
+{
 
+    public function fetchAll()
+    {
+        $sql = 'SELECT books.*, authors.author_name FROM books LEFT JOIN authors ON authors.author_id = books.book_author';
+        $statement = $this->pdo->query($sql);
+        return $statement->fetchAll(\PDO::FETCH_ASSOC);
+    }
+    public function fetchOne($bookId)
+    {
+        $stmt2 = $this->pdo->prepare('SELECT * FROM books WHERE book_id = :book_id');
+        $stmt2->bindParam(':book_id', $bookId, \PDO::PARAM_INT);
+        $stmt2->execute();
+        return $stmt2->fetch(\PDO::FETCH_ASSOC);
+    }
 }
